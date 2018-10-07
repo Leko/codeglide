e66a5e0c2f2637afdd5be804e88ba3ff04dae459
Leko <leko.noor@gmail.com>
2018-10-07 14:23:45 +0900

    :memo: Add ReadOnlyEditor stories

--- packages/components/.storybook/config.js
+++ packages/components/.storybook/config.js
@@ -1,12 +1,14 @@
 import { configure, addDecorator } from "@storybook/react";
 import { withOptions } from "@storybook/addon-options";
 import { withInfo } from "@storybook/addon-info";
+import { withKnobs } from "@storybook/addon-knobs";
 
 function loadStories() {
   require("../stories/index");
 }
 
 addDecorator(withInfo({ inline: true }));
+addDecorator(withKnobs({ escapeHTML: false }));
 addDecorator(
   withOptions({
     addonPanelInRight: true
--- packages/components/stories/index.tsx
+++ packages/components/stories/index.tsx
@@ -1,16 +1,28 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import Button from "../src/molecules/Button";
+import { text, select } from "@storybook/addon-knobs";
+import { ReadOnlyEditor } from "../src/molecules/ReadOnlyEditor";
+const deindent = require("deindent");
 
-storiesOf("Button", module)
-  .add("with text", () => (
-    <Button onClick={action("clicked")}>Hello Button</Button>
-  ))
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf("ReadOnlyEditor", module).add("Show TypeScript code", () => (
+  <ReadOnlyEditor
+    editorDidMount={action("editorDidMount")}
+    language={text("language", "typescript")}
+    width={text("width", "100%")}
+    height={text("height", "200")}
+    theme={select("theme", ["vs-dark", "vs", "hc-black"], "vs-dark")}
+    value={text(
+      "value",
+      deindent`
+    import { Hoge } from './Hoge'
+    const hoge = 2
+    const foo = (some) => (unused) => (variables) => () => () => () => () => () => () => () => () => () => () => () => () => () => {
+      const h = new Hoge()
+      return hoge + 1
+    }
+    foo()
+    `
+    )}
+  />
+));
