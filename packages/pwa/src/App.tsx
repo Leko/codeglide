7115382d96a02cf585907f622130e359d2ba64c2
Leko <leko.noor@gmail.com>
2018-10-17 00:39:45 +0900

    :construction: Configure store and required props

--- packages/pwa/src/App.tsx
+++ packages/pwa/src/App.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import { Routes } from "./routes";
+
+const store = createStore(state => state);
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Routes />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Routes />
+        </div>
+      </Provider>
     );
   }
 }
--- packages/pwa/src/containers/Dashboard.tsx
+++ packages/pwa/src/containers/Dashboard.tsx
@@ -1,7 +1,10 @@
 import { connect } from "react-redux";
 import { Dashboard } from "@codeglide/components";
 
-const mapStateToProps = () => ({});
+const mapStateToProps = () => ({
+  searchHistory: [],
+  repositoryHistory: []
+});
 const mapDispatchToProps = () => ({});
 
 export default connect(
