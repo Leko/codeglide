9dcb468a1af36913af87d1eced7ca6851652f7d6
Leko <leko.noor@gmail.com>
2018-10-16 23:42:42 +0900

    :fire: Remove unused file and imports

--- packages/pwa/src/App.js
+++ packages/pwa/src/App.js
@@ -1,29 +1,8 @@
 import React, { Component } from "react";
-import Editor from "./Editor";
-
-const value = `
-// type your code...
-const hoge = 2
-
-const foo = (some) => (unused) => (variables) => () => () => () => () => () => () => () => () => () => () => () => () => () => {
-  return hoge + 1
-}
-
-foo()
-`;
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Editor
-          value={value}
-          language="typescript"
-          width={window.innerWidth}
-          height={window.innerHeight}
-        />
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
