229b8ebb283b6d85febb158cd2e4d957fcc325ea
Leko <leko.noor@gmail.com>
2018-10-17 00:22:09 +0900

    :heavy_plus_sign: Add redux, react-redux, react-router(DOM)

---
+++ packages/pwa/src/containers/Dashboard.tsx
@@ -0,0 +1,10 @@
+import { connect } from "react-redux";
+import { Dashboard } from "@codeglide/components";
+
+const mapStateToProps = () => ({});
+const mapDispatchToProps = () => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dashboard);
--- packages/pwa/src/routes.tsx
+++ packages/pwa/src/routes.tsx
@@ -1,3 +1,9 @@
 import * as React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Dashboard from "./containers/Dashboard";
 
-export const Routes = () => <div />;
+export const Routes = () => (
+  <Router>
+    <Route exact path="/" component={Dashboard} />
+  </Router>
+);
