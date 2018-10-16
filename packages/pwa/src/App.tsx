70b25325af4891251c4b48771c86a97f7c252c30
Leko <leko.noor@gmail.com>
2018-10-17 00:08:33 +0900

    :wrench: Configure TypeScript

---
+++ packages/pwa/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Routes } from "./routes";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Routes />
+      </div>
+    );
+  }
+}
+
+export default App;
